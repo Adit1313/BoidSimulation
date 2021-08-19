bf6a9052038001f982f02afa1c82ce300e5fd7b0
Adit Vikas Kirtani <aditk@pop-os.localdomain>
2021-08-19 17:26:25 +0530

    Added movement

--- main.py
+++ main.py
@@ -17,23 +17,30 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 birds = []
 
 class Bird:
-    def __init__(self, x, y):
+    def __init__(self, x, y, velX, velY):
         self.x = x
         self.y = y
-    
-def render(b):
-    pygame.draw.rect(screen, (RED, GREEN, BLUE), pygame.Rect(b.x, b.y, BirdWidth, BirdHeight))
+        self.velX = velX
+        self.velY = velY
 
 def init():
     for i in range(0, 25):
-        birds.append(Bird(random.randint(0, WIDTH - BirdWidth), random.randint(0, HEIGHT - BirdHeight)))
+        birds.append(Bird(random.uniform(0, WIDTH - BirdWidth), random.uniform(0, HEIGHT - BirdHeight), random.uniform(-0.5, 0.5), random.uniform(-0.5, 0.5)))
+
+def render(b):
+    pygame.draw.rect(screen, (RED, GREEN, BLUE), pygame.Rect(int(b.x), int(b.y), BirdWidth, BirdHeight))
+
+def update(b):
+    b.x = b.x + b.velX
+    b.y = b.y + b.velY
 
 def loop():
     done = False
     while not done:
+        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, WIDTH, HEIGHT))
         for b in birds:
             render(b)
-            #update(b)
+            update(b)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
